bce5c6e63cbc1dd4a646873027fb84996dcba947
EduardoXT <eduardo.souza8419@gmail.com>
2023-01-26 00:29:53 -0300

    finalizando a aplicação

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function toggleMode() {
-    const body = document.body
+    const html = document.documentElement
+    html.classList.toggle('light')
     
-    if(body.classList.contains)
+    
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', './assets/images/avatar-light.png')
+        img.setAttribute('alt', '"Foto de Eduardo sorrindo timidamente, usando óculos escuro, camisa preta e fundo branco"')
+    } else {
+        img.setAttribute('src', './assets/images/avatar.png')
+        img.setAttribute('alt', '"Foto de Eduardo sorrindo timidamente, usando óculos de grau, camisa preta e fundo branco"')
+    }
+
+
+
+
+
 }
